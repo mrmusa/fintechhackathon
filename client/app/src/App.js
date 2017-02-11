8d6d34603a33e575cb5726e8710226d92e7f2160
Esterling Accime <accimeesterlin@yahoo.com>
2017-02-11 15:29:55 -0500

    Installing React App to the develop branch.

---
+++ client/app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(props){
+  return(
+    <h1>Welcome to Zen Spend!</h1>
+    );
+}
+
+export default App;
