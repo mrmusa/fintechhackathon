61fea13b3b3a5c5517cc089227938e61222326c7
Esterling Accime <accimeesterlin@yahoo.com>
2017-02-11 17:01:27 -0500

    Semanatic UI installed on index.html and jquery cdn installed

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-//import "./styles/App.css";
-function App(props){
-return <h1>Welcome to Zen Spend!</h1>
-}
+import "./styles/App.css";
 
+function App(props){
+  return(
+    <div className="container">
+      <div className="ui raised segment">
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore sint sunt, dolorem quis quod saepe, velit cupiditate omnis culpa ipsum, voluptas asperiores ipsa laudantium porro impedit corporis deleniti, est cumque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim totam asperiores inventore dolor odit temporibus nam consectetur, quod eligendi ut corporis quae eaque quas, debitis, voluptatibus eius vitae perspiciatis modi.</p>
+        </div>
+    </div>
+    );
+}
 
 export default App;
