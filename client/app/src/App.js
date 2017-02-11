7d3419c4f20040067fe6832351c6ab5a669d8ed2
Esterling Accime <accimeesterlin@yahoo.com>
2017-02-11 16:40:52 -0500

    styles added to the client development

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+//import "./styles/App.css";
 function App(props){
-  return(
-    <h1>Welcome to Zen Spend!</h1>
-    );
+return <h1>Welcome to Zen Spend!</h1>
 }
+
 
 export default App;
