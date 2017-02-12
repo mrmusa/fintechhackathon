a1ec764bd202f139f2912de96e1fd6a888381321
Musa Siddeeq <musa.siddeeq@turner.com>
2017-02-12 04:05:42 -0500

    base api server

---
+++ server/api/index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import rp from 'request-promise'
+import templates from 'uri-templates'
+import config from '../config'
+import xTransactions from '../lib/transformers/transaction'
+
+const notImplemented = (req, res, next) => res.status(501).json({ 'error': 'NotImplemented' });
+const router = express.Router();
+
+router.get('/txn/latest', (req, res, next) => {
+    const tsys = config.get('opts:tsys');
+    const cardNumber = '4124120000002351';
+    const dateFrom = '2016-11-01';
+    const dateTo = '2016-11-30';
+    const params = { cardNumber, dateFrom, dateTo };
+    tsys.uri = templates(tsys.uri).fill(params);
+    rp(tsys).then(xTransactions)
+        .then(zsRes => res.status(200).json(zsRes)).catch(next);
+});
+
+router.put('/txn/:txnId', notImplemented);
+
+router.get('/cue/latest', notImplemented);
+
+export default router;
+
---
+++ server/lib/transformers/transaction.js
@@ -0,0 +1,23 @@
+export default (tsysRes) => {
+    const { transactions } = tsysRes;
+
+    return { transactions: transactions.map(txn => {
+        const {
+            date,
+            postingId,
+            amount,
+            code,
+            meta,
+            merchant,
+            postingDate,
+            type,
+            statementBeginDate
+        } = txn;
+
+        return {
+            description: meta.description,
+            merchant,
+            date
+        }
+    })};
+}
