c39c0193f2aa1c72da34b8154696248e5d6850dd
Esterling Accime <accimeesterlin@yahoo.com>
2017-02-12 03:05:12 -0500

    HTTP call made to a demo server

--- client/zenspend/src/pages/home/home.ts
+++ client/zenspend/src/pages/home/home.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: null;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public http: Http) {
+
+	  this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+	        this.posts = data.data.children;
+	        console.log(data.data.children);
+	    });
 
   }
 
 }
+
+
+
